0a3083aaf2187969c73b652150906d6ad6a157c2
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2021-12-25 12:20:19 +0100

    :recycle:

--- run.js
+++ run.js
@@ -1,12 +1,8 @@
-'use strict';
-const request = require('request');
-const _ = require('lodash');
-const fs = require('fs');
+import fetch from 'node-fetch';
+import _ from 'lodash';
+import fs from 'fs';
 
-writeReadme();
-writeAvailable();
-
-function writeReadme() {
+const writeReadme = async () => {
     const url = 'https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json';
 
     const preamble = '# GitHub Emoji\n';
@@ -18,58 +14,49 @@ function writeReadme() {
 
     const postamble = `\n# Build\n\nThis file is generated from the data in ${url}\n\n1. \`npm install\`\n2. \`npm run start\`\n`;
 
-    request.get({
-        url: url,
-        json: true,
-        headers: { 'User-Agent': 'request' }
-    }, (err, res, data) => {
-        if (err) {
-            return console.log('Error:', err);
-        } else if (res.statusCode !== 200) {
-            return console.log('Status:', res.statusCode);
-        } else {
-            let result = preamble;
+    const response = await fetch(url);
+    const data = await response.json();
 
-            var categories = _(data)
-                .groupBy(x => x.category)
-                .map((value, key) => ({ name: key, emojis: value }))
-                .value();
+    let result = preamble;
 
-            for (let index in categories) {
-                let category = categories[index];
-                result += heading.replace('{0}', (category.name === 'undefined') ? 'Uncategorized' : category.name);
-                result += header;
+    let categories = _(data)
+        .groupBy(x => x.category)
+        .map((value, key) => ({ name: key, emojis: value }))
+        .value();
 
-                for (let index in category.emojis) {
-                    let emoji = category.emojis[index];
-                    let aliases = '';
+    for (let index in categories) {
+        let category = categories[index];
+        result += heading.replace('{0}', (category.name === 'undefined') ? 'Uncategorized' : category.name);
+        result += header;
 
-                    for (let index in emoji.aliases) {
-                        if (aliases.length > 0) aliases += ' <br /> ';
-                        aliases += cell.replace('{0}', emoji.aliases[index]);
-                    }
+        for (let index in category.emojis) {
+            let emoji = category.emojis[index];
+            let aliases = '';
 
-                    result += row
-                        .replace('{0}', emoji.aliases[0])
-                        .replace('{1}', emoji.aliases[0])
-                        .replace('{2}', aliases)
-                        .replace('{3}', emoji.description || '')
-                        .replace('{4}', emoji.tags.join(', '));
-                }
+            for (let index in emoji.aliases) {
+                if (aliases.length > 0) aliases += ' <br /> ';
+                aliases += cell.replace('{0}', emoji.aliases[index]);
             }
 
-            result += postamble;
+            result += row
+                .replace('{0}', emoji.aliases[0])
+                .replace('{1}', emoji.aliases[0])
+                .replace('{2}', aliases)
+                .replace('{3}', emoji.description || '')
+                .replace('{4}', emoji.tags.join(', '));
+        }
+    }
 
-            fs.writeFile("README.md", result, function (err) {
-                if (err) {
-                    return console.log('Error:', err);
-                }
-            });
+    result += postamble;
+
+    fs.writeFile("README.md", result, function (err) {
+        if (err) {
+            return console.log('Error:', err);
         }
     });
 }
 
-function writeAvailable() {
+const writeAvailable = async () => {
     const url = 'https://api.github.com/emojis';
 
     const preamble = '# GitHub Emoji\n\n';
@@ -79,32 +66,26 @@ function writeAvailable() {
 
     const postamble = `\n# Build\n\nThis file is generated from the data in ${url}\n\n1. \`npm install\`\n2. \`npm run start\`\n`;
 
-    request.get({
-        url: url,
-        json: true,
-        headers: { 'User-Agent': 'request' }
-    }, (err, res, data) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let result = preamble;
+    result += header;
+
+    for (let alias in data) {
+        result += row
+            .replace('{0}', alias)
+            .replace('{1}', alias);
+    }
+
+    result += postamble;
+
+    fs.writeFile("available.md", result, function (err) {
         if (err) {
             return console.log('Error:', err);
-        } else if (res.statusCode !== 200) {
-            return console.log('Status:', res.statusCode);
-        } else {
-            let result = preamble;
-            result += header;
-
-            for (let alias in data) {
-                result += row
-                    .replace('{0}', alias)
-                    .replace('{1}', alias);
-            }
-
-            result += postamble;
-
-            fs.writeFile("available.md", result, function (err) {
-                if (err) {
-                    return console.log('Error:', err);
-                }
-            });
         }
     });
 }
+
+writeReadme();
+writeAvailable();
